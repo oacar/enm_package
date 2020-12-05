fixed normalized laplacian calculation by default

src/features/huri_enm.py
@@ -11,22 +11,32 @@ from Bio import SeqIO
 figure_path = 'reports/figures/huri_0722'
 huri = Enm('huri')
 huri.read_network('data/interim/huri/huri_cleared.csv')
-huri.gnm_analysis()
+huri.gnm_analysis(normalized=False)
 
 huri.figure_path=figure_path
-huri.output_path = 'data/interim/huri/'
+huri.output_path = 'data/interim/huri_0916/'
 #huri.plot_collectivity()
-
-huri.plot_network_spring()
+huri.spring_pos()
+#huri.plot_network_spring()
 #huri.plot_vector(sorted=True)
 #huri.plot_scatter(x='deg',y='eff',figure_name='deg_eff',figure_extension='pdf')
 #huri.plot_scatter(x='deg',y='sens',figure_name='deg_sens',figure_extension='pdf')
 
-huri.simulate_rewire(output_name='rewire_data',save=True)
-huri.rewire_df
-huri.plot_correlation_density(x='eff',y='deg',figure_extension='pdf')
-huri.plot_correlation_density(x='sens',y='deg',correlation='spearman',figure_extension='pdf')
+huri.simulate_rewire(output_name='rewire_data',save=True,normalized=False)
+#huri.rewire_df
+#huri.plot_correlation_density(x='eff',y='deg',figure_extension='pdf')
+#huri.plot_correlation_density(x='sens',y='deg',correlation='spearman',figure_extension='pdf')
 nx.set_node_attributes(huri.graph_gc,dict(zip(list(huri.graph_gc.nodes),list(huri.graph_gc.nodes))),'orf_name')
+
+neigbor_btw = []
+neighbor_degree = []
+for i in huri.graph_gc.nodes:
+    neigbor_btw.append(np.average([huri.df.loc[huri.df.orf_name==a,'btw'].values for a in nx.neighbors(huri.graph_gc,i)]))
+    neighbor_degree.append(np.average([huri.df.loc[huri.df.orf_name==a,'deg'].values for a in nx.neighbors(huri.graph_gc,i)]))
+
+huri.df['neighbor_btw'] = neigbor_btw
+huri.df['neighbor_degree'] = neighbor_degree
+
 
 with open(f"{huri.output_path}/huri.pickle",'wb') as f:
     pickle.dump(huri,f, protocol=4)

src/features/yuri_enm.py
@@ -11,26 +11,36 @@ from Bio import SeqIO
 figure_path = 'reports/figures/yuri_0519'
 yuri = Enm('yuri')
 yuri.read_network('data/interim/yuri/yuri_combined.csv')
-yuri.gnm_analysis()
+yuri.gnm_analysis(normalized=False)
 
 yuri.figure_path=figure_path
-yuri.output_path = 'data/interim/yuri/'
-yuri.plot_collectivity()
-
-yuri.plot_network_spring()
-yuri.plot_vector(sorted=True)
-yuri.plot_scatter(x='deg',y='eff',figure_name='deg_eff',figure_extension='pdf')
-yuri.plot_scatter(x='deg',y='sens',figure_name='deg_sens',figure_extension='pdf')
-
-yuri.simulate_rewire(output_name='rewire_data',save=True)
-yuri.rewire_df
-yuri.plot_correlation_density(x='eff',y='deg',figure_extension='pdf')
-yuri.plot_correlation_density(x='sens',y='deg',correlation='spearman',figure_extension='pdf')
+yuri.output_path = 'data/interim/yuri_0916/'
+#yuri.plot_collectivity()
+yuri.spring_pos()
+#yuri.plot_network_spring()
+#yuri.plot_vector(sorted=True)
+#yuri.plot_scatter(x='deg',y='eff',figure_name='deg_eff',figure_extension='pdf')
+#yuri.plot_scatter(x='deg',y='sens',figure_name='deg_sens',figure_extension='pdf')
+#
+yuri.simulate_rewire(output_name='rewire_data',save=True,normalized=False)
+##yuri.rewire_df
+#yuri.plot_correlation_density(x='eff',y='deg',figure_extension='pdf')
+#yuri.plot_correlation_density(x='sens',y='deg',correlation='spearman',figure_extension='pdf')
 nx.set_node_attributes(yuri.graph_gc,dict(zip(list(yuri.graph_gc.nodes),list(yuri.graph_gc.nodes))),'orf_name')
+
+neigbor_btw = []
+neighbor_degree = []
+for i in yuri.graph_gc.nodes:
+    neigbor_btw.append(np.average([yuri.df.loc[yuri.df.orf_name==a,'btw'].values for a in nx.neighbors(yuri.graph_gc,i)]))
+    neighbor_degree.append(np.average([yuri.df.loc[yuri.df.orf_name==a,'deg'].values for a in nx.neighbors(yuri.graph_gc,i)]))
+
+yuri.df['neighbor_btw'] = neigbor_btw
+yuri.df['neighbor_degree'] = neighbor_degree
+
 
 with open(f"{yuri.output_path}/yuri.pickle",'wb') as f:
     pickle.dump(yuri,f)
 
-go_df = pd.read_csv('data/interim/go_results/4.tsv','\t')
+#go_df = pd.read_csv('data/interim/go_results/4.tsv','\t')
 
-yuri.plot_network_spring(plot_go=True,go_df_list=[go_df],level_list=[0.2])
+#yuri.plot_network_spring(plot_go=True,go_df_list=[go_df],level_list=[0.2])