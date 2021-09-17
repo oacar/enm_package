2408e77ed892a73da1bf40bb590e114a0087bd69
Omer Acar <omeracar0@gmail.com>
2021-09-17 09:30:02 -0400

    plot heatmap function

--- notebooks/7-different-network-sizes.py
+++ notebooks/7-different-network-sizes.py
@@ -18,6 +18,7 @@ import pandas as pd
 import networkx as nx
 import numpy as np
 import matplotlib.pyplot as plt
+from enm.utils import *
 
 # %% [markdown]
 # # Giant component size change
@@ -37,7 +38,7 @@ ne_ratio_list = []
 nv_ratio_list = []
 
 # %%
-thr_list =  [0.05,0.1,0.2,0.25,0.3,0.35,0.4,0.45,0.5]
+thr_list =  [0.05,0.1,0.2,0.25,0.3,0.35,0.4]
 
 # %%
 for i in np.arange(0.2,0.75,0.05):
@@ -66,28 +67,23 @@ ax.set_xlabel(xlab)
 # %%
 sensor_df_names = [ ]
 
+# %%
+effector_dfs = get_result_dfs('effectors_df', thr_list)
 
 # %%
-def get_result_dfs(fname, thr_list, default_thr):
-    dfs = {thr: pd.read_csv(f"../data/interim_{thr}/{fname}.csv") for thr in thr_list if thr!=default_thr}
-    dfs[default_thr] = pd.read_csv(f"../data/interim/{fname}.csv")
-    if default_thr not in thr_list:
-        thr_list.insert(0, default_thr)                               
-    return dfs
-
+sensor_dfs =  get_result_dfs('sensors_df', thr_list)
 
 # %%
-effector_dfs = get_result_dfs('effectors_df', thr_list, 0.2)
-
-# %%
-sensor_dfs =  get_result_dfs('sensors_df', thr_list, 0.2)
+effector_sensor_go_dfs = get_result_dfs('effector_sensor_combined_go_df',thr_list)
 
 # %%
 sensor_dfs[0.2].dropna(subset=['sensor_cluster']).loc[:,'sensor_cluster'].nunique()
 
 
 # %%
-def plot_go_thr_comparison(dfs, col, yaxis):
+
+# %%
+def plot_go_thr_comparison(dfs, col, yaxis,plotname, xlab='PCC Threshold'):
     n_goterms = []
     rat_goterms = []
     n_clusters = []
@@ -99,45 +95,96 @@ def plot_go_thr_comparison(dfs, col, yaxis):
         n_clusters.append(df.dropna(subset=[col]).loc[:,col].nunique())
         n_go_clusters.append(df.dropna(subset=[col]).loc[:,'go_group'].nunique())
         
-    fig, axs = plt.subplots(1,3,figsize=(15,5))
+    fig, axs = plt.subplots(1,2,figsize=(5,2.5))
     axs[0].plot(thr_list, n_clusters, 'o-')
-    axs[0].set_ylabel(f'Number of {yaxis} clusters')
-    axs[0].set_xlabel(xlab)
+    axs[0].set_ylabel(f'Number of {yaxis} clusters', fontsize=12)
+    axs[0].set_xlabel(xlab, fontsize=12)
     
     axs[1].plot(thr_list, n_go_clusters, 'o-')
-    axs[1].set_ylabel(f'Number of go enriched {yaxis} clusters')
-    axs[1].set_xlabel(xlab)
+    axs[1].set_ylabel(f'Number of go enriched\n{yaxis} clusters', fontsize=12)
+    axs[1].set_xlabel(xlab, fontsize=12)
     
-    axs[2].plot(thr_list, [i/j if i!=0 else 0 for i,j in zip(n_go_clusters,n_clusters) ], 'o-')
-    axs[2].set_ylabel(f'% of go enriched {yaxis} clusters')
-    axs[2].set_xlabel(xlab)
+    plt.tight_layout()
+    fig.savefig(f'../reports/figures/paper_figures_supp/{plotname}.png', bbox_inches='tight', dpi=150)
+    # axs[2].plot(thr_list, [i/j if i!=0 else 0 for i,j in zip(n_go_clusters,n_clusters) ], 'o-')
+    # axs[2].set_ylabel(f'% of go enriched {yaxis} clusters')
+    # axs[2].set_xlabel(xlab)
 
+
+# %%
+plot_go_thr_comparison(sensor_dfs,'sensor_cluster', 'sensor',plotname='thr_num_sensor_clusters')
 
 # %%
 plot_go_thr_comparison(effector_dfs,'effector_cluster', 'effector')
 
 # %%
-plot_go_thr_comparison(sensor_dfs,'sensor_cluster', 'sensor')
+effector_sensor_go_dfs[0.2]
+
+# %%
+help(t.set_text)
+
+# %%
+#effector_sensor_go_dfs
+go_overlap = pd.DataFrame({thr : [len(np.intersect1d(effector_sensor_go_dfs[thr].loc[effector_sensor_go_dfs[thr].cluster_type=='effector','GO'], effector_sensor_go_dfs[i].loc[effector_sensor_go_dfs[i].cluster_type=='effector','GO'])) for i in thr_list] for thr in sorted(thr_list)})
+go_overlap.index = thr_list
+def plot_heatmap_overlap(df, filename, title , cmap='YlGnBu' , figsize = (10,6)):
+    import seaborn as sns
+    mask = np.zeros_like(df)
+    mask[np.tril_indices_from(mask, k=-1)] = True
+    with sns.axes_style("white"):
+        f, ax = plt.subplots(figsize=figsize)
+        ax = sns.heatmap(100*df/ np.diag(df),annot=True, mask= mask, square=True, vmax=100, cbar=True, ax=ax, fmt='.1f',cmap=cmap)
+        ax.set_title(title)
+        
+        for t in ax.texts: 
+            t.set_text(t.get_text() + "%")
+            t.set_size(9)
+        ax.set_xlabel(ax.get_xlabel(),fontsize=16)
+    plt.savefig(f'../reports/figures/paper_figures_supp/{filename}.png', bbox_inches='tight',dpi=150)    
+
+plot_heatmap_overlap(go_overlap, 'go_overlap_test', 'GO term overlap for effectors\nin different thresholds', figsize=(5,5))
+
 
 # %%
 
 sensor_overlap = pd.DataFrame({thr : [len(np.intersect1d(sensor_dfs[thr].orf_name, sensor_dfs[i].orf_name)) for i in thr_list] for thr in sorted(thr_list)})
 sensor_overlap.index = thr_list
 
-# %% [markdown]
-# # Sensor overlap between thresholds
+effector_overlap = pd.DataFrame({thr : [len(np.intersect1d(effector_dfs[thr].orf_name, effector_dfs[i].orf_name)) for i in thr_list] for thr in sorted(thr_list)})
+effector_overlap.index = thr_list
 
 # %%
-sns.heatmap(sensor_overlap / np.diag(sensor_overlap))
-
-# %% [markdown]
-# # Effector overlap between thresholds
+plot_heatmap_overlap(sensor_overlap, 'sensor_overlap', 'Sensor overlap for different thresholds', figsize=(5,5))
 
 # %%
+plot_heatmap_overlap(effector_overlap, 'effector_overlap', 'Effector overlap for different thresholds', figsize=(5,5))
+
+# %%
+
+sensor_overlap = pd.DataFrame({thr : [len(np.intersect1d(sensor_dfs[thr].orf_name, sensor_dfs[i].orf_name)) for i in thr_list] for thr in sorted(thr_list)})
+sensor_overlap.index = thr_list
 
 effector_overlap = pd.DataFrame({thr : [len(np.intersect1d(effector_dfs[thr].orf_name, effector_dfs[i].orf_name)) for i in thr_list] for thr in sorted(thr_list)})
 effector_overlap.index = thr_list
-sns.heatmap(effector_overlap / np.diag(effector_overlap))
+
+mask1 = np.zeros_like(sensor_overlap)
+mask1[np.tril_indices_from(mask1, k=-1)] = True
+mask2 = np.zeros_like(effector_overlap)
+mask2[np.tril_indices_from(mask2,k=-1)] = True
+with sns.axes_style("white"):
+    f, ax = plt.subplots(1,3,figsize=(16, 6), gridspec_kw={'width_ratios': [10,10, 1]})
+    ax[0] = sns.heatmap(100*sensor_overlap / np.diag(sensor_overlap),annot=True, mask=mask1, square=True, vmax=100, cbar=False, ax=ax[0], fmt='.1f',cmap="YlGnBu")
+    ax[0].set_title('Sensor overlap for different thresholds')
+    for t in ax[0].texts: t.set_text(t.get_text() + "%")
+    ax[1] = sns.heatmap(100 * effector_overlap / np.diag(effector_overlap),annot=True, mask=mask2, square=True, ax=ax[1],cbar_ax=ax[2], vmax=100, fmt='.1f',cmap="YlGnBu")
+    for t in ax[1].texts: t.set_text(t.get_text() + "%")
+    ax[1].set_title('Effector overlap for different thresholds')
+    ax[0].text(-0.1, -1, 'A', fontsize=16, fontweight='bold',va='top',ha='right')
+    ax[1].text(-0.1, -1, 'B', fontsize=16, fontweight='bold',va='top',ha='right')
+
+
+plt.tight_layout()
+plt.savefig('../reports/figures/paper_figures_supp/figs3.png', bbox_inches='tight',dpi=150)
 
 # %%
 import itertools as itr
@@ -151,19 +198,6 @@ def get_pairwise_intersect(dfs, thr_list):
 
     return res
 
-
-# %% jupyter={"outputs_hidden": true}
-for i in range(1,len(thr_list)):
-    prev = thr_list[i-1]
-    curr = thr_list[i]
-    print(prev,curr)
-    curr_sensors = sensor_dfs[curr].orf_name.tolist()
-    pcc_dfs[prev]['sensor_on_larger_thr'] = pd.Categorical([1 if i else 0 for i in pcc_dfs[prev].orf_name.isin(curr_sensors)])
-    
-    fig, ax = plt.subplots(figsize=(6,6))
-    pcc_dfs[prev].loc[pcc_dfs[prev].sensor_on_larger_thr==1].plot.scatter('deg','sens',c='sensor_on_larger_thr',cmap='viridis',ax=ax)
-    ax.set_title(prev)
-    ax.set_ylim(pcc_dfs[prev].sens.min(),pcc_dfs[prev].sens.max())
 
 # %% jupyter={"outputs_hidden": true}
 get_pairwise_intersect(effector_dfs, thr_list)
@@ -206,110 +240,6 @@ from sklearn.decomposition import PCA
 import seaborn as sns
 
 
-
-# %% [markdown]
-# # Redefine sensors/effectors
-
-# %%
-# %load_ext rpy2.ipython
-
-# %%
-thr = 0.2
-dff = pcc_dfs[thr].drop('sensor_on_larger_thr',axis=1)
-
-
-# %%
-#Import the SignatureTranslatedAnonymousPackage 
-from rpy2.robjects.packages import STAP
-import rpy2.robjects as ro
-#Read the file with the R code snippet
-with open('../R/compare_clustering.R', 'r') as f:
-    string = f.read()
-#Parse using STAP
-r_func = STAP(string, "compare_clustering")
-
-# %% language="R"
-#
-# library(Ckmeans.1d.dp)
-# library(tidyverse)
-# library(glue)
-# compare_clustering <- function(filename, number_of_clusters, column_name){
-#     df <- readr::read_csv(filename)
-#     res <- Ckmeans.1d.dp(pull(df[column_name]),number_of_clusters)
-#   #  plotBIC(res)
-#     df$ck = res$cluster
-#   #  df$color = df$ck#>quantile(unique(df$ck),quantile_threshold)
-# #     ggplot(df, aes_string(x='deg',y=column_name,color="color"))+
-# #         geom_point()
-#     return(df)
-# }
-
-# %%
-from enm.utils import *
-#goea, geneid2name = create_goea()
-
-# %%
-def get_go_enrichment_by_cluster(clustered_df, cluster_column_name, goea, geneid2name):
-    go_res = {}
-    for i in sorted(clustered_df[cluster_column_name].unique()):
-        orf_list = clustered_df.loc[clustered_df[cluster_column_name]==i]
-        go_df = query_goatools(orf_list, goea, geneid2name)
-        go_res[i] = go_df
-    return go_res
-
-
-# %%
-def go_for_a_threshold(thr, column_name,goea, geneid2name):
-    #%%R -i dff -i thr -o clustered_df
-    clustered_df= convert_rpy2_to_pandas(r_func.compare_clustering(f'../data/interim_{thr}/pcc_df.csv',ro.IntVector((2,50)), column_name))
-    go_res = get_go_enrichment_by_cluster(clustered_df, 'ck', goea, geneid2name)
-    return clustered_df, go_res
-
-
-# %% jupyter={"outputs_hidden": true}
-clustered_dfs_sens = {}
-clustered_dfs_eff = {}
-
-go_res_all_sens = {}
-go_res_all_eff = {}
-
-for i in thr_list:
-    clustered_df_sens, go_res_sensor = go_for_a_threshold(i, 'sens',goea, geneid2name)
-    clustered_df_eff, go_res_eff = go_for_a_threshold(i, 'eff',goea, geneid2name)
-    clustered_dfs_sens[i] = clustered_df_sens
-    clustered_dfs_eff[i] = clustered_df_eff
-    go_res_all_sens[i] = go_res_sensor
-    go_res_all_eff[i] = go_res_eff
-
-
-# %% jupyter={"outputs_hidden": true}
-clustered_df_sens, go_res_sensor_02 = go_for_a_threshold(thr, 'sens',goea, geneid2name)
-clustered_df_eff, go_res_eff_02 = go_for_a_threshold(thr, 'eff',goea, geneid2name)
-
-# %%
-go_res_eff_02[10]
-
-# %% [raw]
-# def plot_kmeans(data,x,y):
-#     kmeans = KMeans(init="k-means++", n_clusters=5, n_init=1,
-#                 random_state=0)
-#     X = np.array(data[y]).reshape(-1,1)
-#     y_km = kmeans.fit_predict(X)
-#     data['kmeans'] = y_km
-#     data.groupby('kmeans').sum().loc[y]
-#     sns.scatterplot(data=data,x=x,y=y,hue=y_km)
-
-# %%
-data =pcc_dfs[0.2].copy()
-data['kmeans'] = y_km
-
-
-# %%
-data.groupby('kmeans').sum()['eff'].argmax()
-
-# %%
-idx =0.45
-plot_kmeans(pcc_dfs[idx], 'deg','sens')
 
 # %% [markdown]
 # # Sensor change in different thresholds
