d95f95325ecb4b97e2f9a6849d4510b5f03cd435
Omer Acar <omeracar0@gmail.com>
2021-09-17 09:32:49 -0400

    cleared supp figure notebook

--- notebooks/7-different-network-sizes.py
+++ notebooks/7-different-network-sizes.py
@@ -81,8 +81,6 @@ sensor_dfs[0.2].dropna(subset=['sensor_cluster']).loc[:,'sensor_cluster'].nuniqu
 
 
 # %%
-
-# %%
 def plot_go_thr_comparison(dfs, col, yaxis,plotname, xlab='PCC Threshold'):
     n_goterms = []
     rat_goterms = []
@@ -115,13 +113,7 @@ def plot_go_thr_comparison(dfs, col, yaxis,plotname, xlab='PCC Threshold'):
 plot_go_thr_comparison(sensor_dfs,'sensor_cluster', 'sensor',plotname='thr_num_sensor_clusters')
 
 # %%
-plot_go_thr_comparison(effector_dfs,'effector_cluster', 'effector')
-
-# %%
-effector_sensor_go_dfs[0.2]
-
-# %%
-help(t.set_text)
+plot_go_thr_comparison(effector_dfs,'effector_cluster', 'effector',plotname='thr_num_effector_clusters')
 
 # %%
 #effector_sensor_go_dfs
@@ -158,141 +150,5 @@ plot_heatmap_overlap(sensor_overlap, 'sensor_overlap', 'Sensor overlap for diffe
 
 # %%
 plot_heatmap_overlap(effector_overlap, 'effector_overlap', 'Effector overlap for different thresholds', figsize=(5,5))
-
-# %%
-
-sensor_overlap = pd.DataFrame({thr : [len(np.intersect1d(sensor_dfs[thr].orf_name, sensor_dfs[i].orf_name)) for i in thr_list] for thr in sorted(thr_list)})
-sensor_overlap.index = thr_list
-
-effector_overlap = pd.DataFrame({thr : [len(np.intersect1d(effector_dfs[thr].orf_name, effector_dfs[i].orf_name)) for i in thr_list] for thr in sorted(thr_list)})
-effector_overlap.index = thr_list
-
-mask1 = np.zeros_like(sensor_overlap)
-mask1[np.tril_indices_from(mask1, k=-1)] = True
-mask2 = np.zeros_like(effector_overlap)
-mask2[np.tril_indices_from(mask2,k=-1)] = True
-with sns.axes_style("white"):
-    f, ax = plt.subplots(1,3,figsize=(16, 6), gridspec_kw={'width_ratios': [10,10, 1]})
-    ax[0] = sns.heatmap(100*sensor_overlap / np.diag(sensor_overlap),annot=True, mask=mask1, square=True, vmax=100, cbar=False, ax=ax[0], fmt='.1f',cmap="YlGnBu")
-    ax[0].set_title('Sensor overlap for different thresholds')
-    for t in ax[0].texts: t.set_text(t.get_text() + "%")
-    ax[1] = sns.heatmap(100 * effector_overlap / np.diag(effector_overlap),annot=True, mask=mask2, square=True, ax=ax[1],cbar_ax=ax[2], vmax=100, fmt='.1f',cmap="YlGnBu")
-    for t in ax[1].texts: t.set_text(t.get_text() + "%")
-    ax[1].set_title('Effector overlap for different thresholds')
-    ax[0].text(-0.1, -1, 'A', fontsize=16, fontweight='bold',va='top',ha='right')
-    ax[1].text(-0.1, -1, 'B', fontsize=16, fontweight='bold',va='top',ha='right')
-
-
-plt.tight_layout()
-plt.savefig('../reports/figures/paper_figures_supp/figs3.png', bbox_inches='tight',dpi=150)
-
-# %%
-import itertools as itr
-
-
-# %%
-def get_pairwise_intersect(dfs, thr_list):
-    pairs = itr.combinations(thr_list,2)
-    nt = lambda a, b: [len(np.intersect1d(dfs[a].orf_name,effector_dfs[b].orf_name)) , len(dfs[b].orf_name)]
-    res = dict([ (t, nt(*t)) for t in pairs ])
-
-    return res
-
-
-# %% jupyter={"outputs_hidden": true}
-get_pairwise_intersect(effector_dfs, thr_list)
-
-# %% jupyter={"outputs_hidden": true}
-get_pairwise_intersect(sensor_dfs, thr_list)
-
-# %%
-pcc_dfs = get_result_dfs('pcc_df', thr_list,0.2)
-
-# %%
-pcc_dfs[i].corr().loc['deg','eff']
-
-# %%
-[pcc_dfs[i].corr('pearson').loc['deg','sens'] for i in thr_list]
-
-# %% jupyter={"outputs_hidden": true}
-for i in thr_list:
-    fig, ax = plt.subplots(figsize=(5,5))
-    pcc_dfs[i].plot.scatter('deg','eff',ax=ax)
-    ax.set_title(i)
-
-# %% jupyter={"outputs_hidden": true}
-for i in thr_list:
-    fig, ax = plt.subplots(figsize=(5,5))
-    pcc_dfs[i].plot.scatter('deg','sens',ax=ax)
-    ax.set_title(i)
-
-# %% jupyter={"outputs_hidden": true}
-sensor_dfs[0.3]
-
-# %%
-
-# %% [raw]
-# %config Completer.use_jedi = False
-
-# %%
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-import seaborn as sns
-
-
-
-# %% [markdown]
-# # Sensor change in different thresholds
-
-# %%
-all_df = pd.read_csv('../data/raw/Data File S3. Genetic interaction profile similarity matrices/cc_ALL.txt','\t',index_col=[0,1], header=[0,1])
-
-# %%
-df_long_renamed = all_df.melt(ignore_index=False, col_level=0).reset_index(inplace=False).rename(columns = {'level_0':'gene1','level_1':'Systematic gene name', 'variable':'gene2','value':'pcc'})
-
-
-# %%
-print("PCC threshold","Sensors in the higher threshold network","Sensors in the higher threshold GC","Number of sensors")
-for i in range(1,len(thr_list)):
-    prev = thr_list[i-1]
-    curr = thr_list[i]
-    
-    sensor_names = sensor_dfs[prev].orf_name
-    all_edges = df_long_renamed.loc[(df_long_renamed.pcc>=curr)]
-    all_nodes = np.unique([*all_edges.gene1, *all_edges.gene2])
-    
-    print(prev, sum(sensor_names.isin(all_nodes)), sum(sensor_names.isin(pcc_dfs[curr].orf_name)) , len(sensor_names))
-
-
-# %%
-for i in thr_list:
-    sensor_names = sensor_dfs[i].orf_name
-    all_edges = df_long_renamed.loc[df_long_renamed.pcc>=i]
-    g = nx.from_pandas_edgelist(all_edges,source='gene1',target='gene2',edge_attr='pcc')
-    fig, ax = plt.subplots(figsize=(7,7))
-    data = [val for u,v,val in g.edges(sensor_names.tolist(), data='pcc')]
-    ax.hist(data)
-    ax.set_title(i)
-    print(i , np.median(data), len(g.nodes))
-
-# %%
-exn = pd.read_csv('../../pgsNetwork/analysis/data/derived_data/SGA_data_combined.csv','\t')
-
-# %%
-aa = exn.loc[exn['Query allele name'].isin(effector_dfs[0.2].orf_name) & exn['Array allele name'].isin(effector_dfs[0.2].orf_name) ]
-
-# %%
-exe_names = np.unique([*aa.loc[aa.data_source=='ExE']['Query allele name'],*aa.loc[aa.data_source=='ExE']['Array allele name']])
-nxe_names = np.unique(aa.loc[aa.data_source=='ExN_NxE']['Array allele name'])
-
-# %%
-strain_ids = pd.read_csv('../../pgsNetwork/trymake/pgsNetwork//analysis/data/derived_data/strain_ids_with_experiment_count_all.csv')
-
-# %%
-strain_ids
-
-# %%
-ii = 0.2
-effector_dfs[ii].loc[effector_dfs[ii].orf_name.isin(strain_ids.loc[strain_ids.maincat=='essential']['Allele Gene name'])].shape[0]/effector_dfs[ii].shape[0]
 
 # %%
